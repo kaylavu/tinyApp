4baf0e2c1503b19651b09da018d631ccc10023c4
kaylavu <kaylaavu@gmail.com>
2018-06-12 23:59:15 +0000

    added route for post request on form , added generate random string function

--- express_server.js
+++ express_server.js
@@ -1,18 +1,28 @@
 var express = require("express"); 
 var app = express(); 
 var PORT = 8080; //default port 8080
+const bodyParser = require("body-parser"); // allows access to POST request parameters 
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca", 
     "9sm5xK": "http://www.google.com"
 }; 
 
+function generateRandomString() {
+    const randomString = Math.random().toString(36).substring(2, 8); 
+    return randomString; 
+}
+
+
 app.get("/", (req,res) => {
     res.end("Hello!"); 
 }); 
 
+//activates listener for server 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`)
 }); 
@@ -30,10 +40,20 @@ app.get("/urls", (req,res) => {
     res.render("urls_index", templateVars); 
 }); 
 
+//adding route handler to render the page with the form 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // debug statement to see POST parameters
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
+
+//example of how res.render looks into the second param 
 //res.render("urls_index", {urls: urlDatabase, key2: 'value2', key3: 'value3})
 //res.render("urls_index", var urls = urlDatabse, var key2 = 'value2, var key3 = 'value3') 
-
-
 app.get("/urls/:id", (req, res) => {
     let longURL = urlDatabase[req.params.id]; 
     console.log(urlDatabase);
@@ -44,6 +64,10 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
   });
 
+
+
+  
+ 
   
 
 
